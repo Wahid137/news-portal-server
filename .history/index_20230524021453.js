71ae824b9e3b05f0a5d26274e5d108efef8aaf5a
wahid-ahmed <wahidahmedshanto@gmail.com>
2023-05-24 12:13:09 +0600

    inital setup and data added

---
+++ .history/index_20230524021453.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+const categories = require("./data/categories.json");
+const news = require("./data/news.json")
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("News API is Running")
+})
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == "08") {
+        res.send(news)
+    }
+})
+
+app.listen(port, () => {
+    console.log("News server is running on port", port)
+})
---
+++ .history/index_20230524021958.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+const categories = require("./data/categories.json");
+const news = require("./data/news.json")
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("News API is Running")
+})
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == "08") {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews)
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.listen(port, () => {
+    console.log("News server is running on port", port)
+})
